988b41178f8b61ea6504499ff646498402b3df0f
Rama Kocherlakota <Rama.Kocherlakota@coxautoinc.com>
2019-01-22 16:49:53 -0500

    include session_token in request

--- bash-container.sh
+++ bash-container.sh
@@ -3,6 +3,7 @@ docker run \
        --name $1 \
        -e AWS_REGION=${AWS_REGION} \
        -e AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} \
+       -e AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN} \
        -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
        -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
        -it -v ${PWD}:/work \
--- py-container.sh
+++ py-container.sh
@@ -4,6 +4,7 @@ docker run \
        -e AWS_REGION=${AWS_REGION} \
        -e AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} \
        -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+       -e AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN} \
        -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
        -it -v ${PWD}:/work \
        -w /work \
